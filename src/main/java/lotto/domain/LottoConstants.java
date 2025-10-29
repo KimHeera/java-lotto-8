0fcee50085450fc58f8348c8da967193535e0531
kimheera <hrk0295@naver.com>
2025-10-29 18:34:22 +0900

    feat: 상수 클래스 구현

---
+++ src/main/java/lotto/domain/LottoConstants.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+public class LottoConstants {
+    public static final int MIN_NUM = 1;
+    public static final int MAX_NUM = 45;
+    public static final int SIZE = 6;
+
+    public static final int LOTTO_PRICE = 1000;
+
+    //output 관련 상수
+    public static final String WINNING_MSG = "당첨 통계";
+    public static final String SEPARATOR = "---";
+    public static final String DELIMITER = ", ";
+    public static final String ERROR_PREFIX = "[ERROR] ";
+
+    //수익률 관련 상수
+    public static final int PERCENTAGE_MUL = 100;
+
+    private LottoConstants(){};
+}
