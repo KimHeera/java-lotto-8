bca09c4c49d748c4ec6da50f64d510104c4ed5cc
kimheera <hrk0295@naver.com>
2025-11-03 17:26:30 +0900

    feat: Lotto.java 클래스 구현
    로또 번호 담는 리스트는 불변 객체로 설계,
    번호 검증과 정렬, 당첨 번호화의 일치 개수 계산

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,37 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+        LottoValidator.validateLottoNum(numbers);
+
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        this.numbers = Collections.unmodifiableList(sortedNumbers);
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+    /*
+    * 다른 Lotto와 비교하여 일치하는 번호 개수를 계산
+    * @param winningLotto 비교 대상 Lotto 객체
+    * @return 일치하는 번호 개수(int)
+     */
+    public int match(Lotto winningLotto){
+        return (int)numbers.stream()
+                .filter(winningLotto.numbers::contains)
+                .count();
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }
