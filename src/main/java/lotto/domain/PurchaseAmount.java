201ad5cb08964a2d8eefd50087244da5d295dd20
kimheera <hrk0295@naver.com>
2025-11-03 16:27:43 +0900

    feat(purchase amount): 입력된 금액 저장, 검증하고 구매 가능한 로또 수량 계산하는 함수 구현

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -1,5 +1,20 @@
 package lotto.domain;
 
 public class PurchaseAmount {
+    private final int amount;
 
+    public PurchaseAmount(String inputAmount){
+        LottoValidator.validatePurchaseAmount(inputAmount);
+
+        inputAmount = inputAmount.trim();
+        this.amount = Integer.parseInt(inputAmount);
+    }
+
+    public int getLottoCnt(){
+        return amount / LottoConstants.PRICE;
+    }
+
+    public int getAmount(){
+        return amount;
+    }
 }
