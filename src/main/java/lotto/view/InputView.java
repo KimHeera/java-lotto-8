de28031bf6c8658b57b2459a40f829a4e26e52c2
kimheera <hrk0295@naver.com>
2025-11-03 18:24:45 +0900

    refactor(InputView): 개행 문자 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,16 +5,26 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public String purchaseAmount(){
         System.out.println("구입금액을 입력해 주세요.");
-        return Console.readLine();
+        String input = Console.readLine();
+        printNewLine();
+        return input;
     }
 
     public String inputOfWinningNum(){
         System.out.println("당첨 번호를 입력해 주세요.");
-        return Console.readLine();
+        String input = Console.readLine();
+        printNewLine();
+        return input;
     }
 
     public String inputOfBonusNum(){
         System.out.println("보너스 번호를 입력해 주세요.");
-        return Console.readLine();
+        String input = Console.readLine();
+        printNewLine();
+        return input;
+    }
+
+    private void printNewLine() {
+        System.out.println();
     }
 }
